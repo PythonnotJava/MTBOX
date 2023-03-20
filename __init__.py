a7aba5808619ad4b51de361a68f346033463191d
PythonnotJava <88701385+PythonnotJava@users.noreply.github.com>
2023-03-20 16:37:05 +0800

    Update __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from PyQt5.QtWidgets import QApplication as QApplication
+from .CodeViewer import ViewSeletedItemCodes as ViewSeletedItemCodes
+from .MT import MatTutorial as MatTutorial
+from .Setting import ReSetDiag as ReSetDiag
+
+__version__ = "1.0.6"
