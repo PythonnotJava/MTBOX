4eb638ddfeaf572c298d22897883ef51322c7f1e
PythonnotJava <88701385+PythonnotJava@users.noreply.github.com>
2023-03-20 16:37:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,39 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mtbox',
-    version='1.0',
-    description='A matplotlib toolbox dedicated to quick start and easy expansion, based on pyqt.',
-    author='PythonnotJava',
-    packages=find_packages(),
+    name='matbox',
+    version='1.0.6',
+    packages= find_packages(),
+    include_package_data=True,
+    package_data={
+        'matbox': [
+            'src/cfg/*.yaml',
+            'src/img/*.ico',
+            'src/mouseCursor/*.cur',
+            'src/QSS/*.qss',
+            'src/tutorial/*.md',
+            'src/tutorial/*.py'
+        ]
+
+    },
+    install_requires=[
+        'PyQt5',
+        'qutepart',
+        'pyyaml',
+        'qt_material',
+        'numpy',
+        'matplotlib'
+    ],
     entry_points={
         'console_scripts': [
-            'mtbox=MT:main',
+            'matbox=MT:main',
         ],
     },
+
+    author='PythonnotJava',
+    author_email='',
+    description='A matplotlib toolbox dedicated to quick start and easy expansion, based on pyqt.',
+    url='https://github.com/PythonnotJava/MTBOX',
+    license='MIT',
+
 )
